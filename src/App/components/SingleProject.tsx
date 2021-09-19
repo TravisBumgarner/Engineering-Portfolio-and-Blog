930b92653c90c2afff9ac1b99e731b5ddd480fb5
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-09-19 17:39:11 -0600

    Revert "updated some outdated bits"
    This reverts commit 22c919549b90d815f63382f9f5630b86cee2d3c8.

--- src/App/components/SingleProject.tsx
+++ src/App/components/SingleProject.tsx
@@ -21,10 +21,6 @@ const Row = styled.div`
 
 const Content = styled.div`
     flex-grow: 1;
-
-    ${media.tablet} {
-        margin: 0 20px;
-    }
 `
 
 const SubContent = styled.div`
@@ -52,7 +48,7 @@ const Sidebar = styled.div`
         min-width: 200px;
     }
 
-    ${media.tablet} {
+    ${media.phone} {
         display: none;
     }
 `
--- webpack.js
+++ webpack.js
@@ -1,53 +1,49 @@
-const path = require("path");
-var webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = (env) => {
-  return {
-    entry: {
-      app: "./src/index.tsx",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.[jt]s[x]?$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
+module.exports = env => {
+    return {
+        entry: {
+            app: './src/index.tsx'
         },
-      ],
-    },
-    output: {
-      filename: "[name]-[hash].bundle.js",
-      path: path.resolve(__dirname, "public"),
-      publicPath: "/",
-    },
-    resolve: {
-      alias: {
-        SharedComponents: path.resolve(__dirname, "src/sharedComponents/"),
-        Theme: path.resolve(__dirname, "src/theme.ts"),
-        Content: path.resolve(__dirname, "src/content"),
-      },
-      extensions: [".ts", ".tsx", ".js"],
-    },
-    devServer: {
-      port: 3000,
-      historyApiFallback: true,
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        __MEDIA__: '"https://storage.googleapis.com/eng40/media/"',
-      }),
-      new webpack.DefinePlugin({
-        __STATIC__: '"https://storage.googleapis.com/eng40/static/"',
-      }),
-      new webpack.DefinePlugin({
-        __IS_PRODUCTION__: env.NODE_ENV === "production",
-      }),
-      new HtmlWebpackPlugin({
-        template: "./src/index.template.ejs",
-        favicon: "./src/favicon.png",
-        inject: "body",
-      }),
-    ],
-  };
-};
+        module: {
+            rules: [
+                {
+                    test: /\.[jt]s[x]?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                }
+            ]
+        },
+        output: {
+            filename: '[name]-[hash].bundle.js',
+            path: path.resolve(__dirname, 'public'),
+            publicPath: '/'
+        },
+        resolve: {
+            alias: {
+                SharedComponents: path.resolve(__dirname, 'src/sharedComponents/'),
+                Theme: path.resolve(__dirname, 'src/theme.ts'),
+                Content: path.resolve(__dirname, 'src/content')
+            },
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        devServer: {
+            contentBase: './public',
+            port: 3000,
+            historyApiFallback: true,
+            publicPath: '/'
+        },
+        plugins: [
+            new webpack.DefinePlugin({ __MEDIA__: '"https://storage.googleapis.com/eng40/media/"' }),
+            new webpack.DefinePlugin({ __STATIC__: '"https://storage.googleapis.com/eng40/static/"' }),
+            new webpack.DefinePlugin({ __IS_PRODUCTION__: env.NODE_ENV === 'production' }),
+            new HtmlWebpackPlugin({
+                template: './src/index.template.ejs',
+                favicon: "./src/favicon.png",
+                inject: 'body'
+            })
+        ]
+    }
+}
