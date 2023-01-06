5f7b95e21e71f19f81affa68aa3c1109cdc6e6f0
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-05 19:27:57 -0700

    more refactor

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,16 +1,13 @@
-import file_53 from './2d-plotter-the-final-attempts.json'
+import file_53 from './2d-plotter-explorations.json'
 import file_11 from './automated-film-scanner.json'
-import file_54 from './engineering-portfolio-v4-2.json'
-import file_38 from './i-make-things-blog.json'
+import file_54 from './engineering-portfolio.json'
 import file_22 from './intro-to-arduino-workshop.json'
 import file_37 from './laser-cut-puzzle.json'
 import file_13 from './painless-prototyping.json'
 import file_14 from './pan-tilt-camera-rig.json'
-import file_40 from './photography-portfolio-v2-0.json'
-import file_39 from './search-engine.json'
+import file_40 from './photography-portfolio.json'
 import file_23 from './security-system.json'
 import file_42 from './smart-display.json'
-import file_48 from './stitch-it-image-to-css-converter.json'
 import file_41 from './strangerer-things-lights.json'
 import file_15 from './timelapse-camera.json'
 import file_49 from './twitch-stream.json'
@@ -47,12 +44,9 @@ const projects: Project[] = [
     file_22,
     file_23,
     file_37,
-    file_38,
-    file_39,
     file_40,
     file_41,
     file_42,
-    file_48,
     file_49,
     file_53,
     file_54,
