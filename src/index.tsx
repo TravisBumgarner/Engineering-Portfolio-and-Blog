f47e2421290738e8e018563a7ada40472aa08cad
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-05 18:02:25 -0700

    upgraded outdated versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,27 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import * as Sentry from '@sentry/browser'
+import { ErrorBoundary, init as sentryInit } from "@sentry/react";
+import { createRoot } from 'react-dom/client';
+
 
 import { Error } from './sharedComponents'
 import App from './App'
 
 if (__IS_PRODUCTION__) {
-    Sentry.init({
+    sentryInit({
         dsn: 'https://96b0481bcf204abfaec06fb7ffc59ed8@sentry.io/1315175'
     })
 }
 
-class SentryWrapper extends Component<{}, { error: null | string }> {
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            error: null
-        }
-    }
 
-    componentDidCatch(error: any, errorInfo: any) {
-        this.setState({ error })
-        Sentry.withScope(scope => {
-            Object.keys(errorInfo).forEach(key => {
-                scope.setExtra(key, errorInfo[key])
-            })
-            Sentry.captureException(error)
-        })
-    }
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-    render() {
-        if (this.state.error) {
-            return <Error />
-        } else {
-            return this.props.children
-        }
-    }
-}
-
-ReactDOM.render(
-    <SentryWrapper>
+root.render(
+    <ErrorBoundary fallback={<Error />}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </SentryWrapper>,
-    document.getElementById('root')
+    </ErrorBoundary>
 )
