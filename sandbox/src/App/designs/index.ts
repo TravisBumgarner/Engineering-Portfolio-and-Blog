292e931db62fa2bfc83b7d321c71a3f6e884416b
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-06 20:32:23 -0700

    progress towards markdown

--- sandbox/src/App/designs/index.ts
+++ sandbox/src/App/designs/index.ts
@@ -2,10 +2,12 @@ import Scattered from "./Scattered";
 import Orderly from "./Orderly";
 import Landing from "./Landing";
 import Orderlyv2 from "./Orderlyv2";
+import MarkdownExperiment from "./MarkdownExperiment";
 
 export default {
     Scattered,
     Orderly,
     Landing,
-    Orderlyv2
+    Orderlyv2,
+    MarkdownExperiment,
 }
