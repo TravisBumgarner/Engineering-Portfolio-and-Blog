97e90a81366e40d62de48f1d4501e6935f9b82a6
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-08 17:04:15 -0700

    remove dead code

--- current/src/sharedComponents/index.tsx
+++ current/src/sharedComponents/index.tsx
@@ -2,6 +2,5 @@ import Text from './Text'
 import Title from './Title'
 import ExternalLink from './ExternalLink'
 import Error from './Error'
-import Markdown from './Markdown'
 
-export { ExternalLink, Text, Error, Title, Markdown }
+export { ExternalLink, Text, Error, Title }
