6617d358f9d0f0a2e83abbc674e414c35649e48c
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-21 12:09:44 -0800

    small deployment tweaks

--- app/src/App/components/Snapshots.tsx
+++ app/src/App/components/Snapshots.tsx
@@ -9,7 +9,7 @@ const TOTAL_PHOTOS = 74
 const Snapshots = () => {
   const photos = useMemo(() => {
     const output = []
-    for (let i = 1; i <= TOTAL_PHOTOS; i++) output.push(<Snapshot key={i} src={`/public/snapshots/snapshot-${i}.jpg`} />)
+    for (let i = 1; i <= TOTAL_PHOTOS; i++) output.push(<Snapshot key={i} src={`${__STATIC__}/snapshots/snapshot-${i}.jpg`} />)
     return shuffledArray(output)
   }, [])
 
