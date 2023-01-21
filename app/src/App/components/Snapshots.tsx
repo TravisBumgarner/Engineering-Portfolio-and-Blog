7844394bae7c32727967cceca520d9e0d65a6f5a
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-01-21 11:47:29 -0800

    Refactored, added new snapshots

---
+++ app/src/App/components/Snapshots.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from 'react'
+
+import { Snapshot } from "SharedComponents"
+
+const shuffledArray = (array: any[]) => array.sort((a, b) => 0.5 - Math.random());
+
+const TOTAL_PHOTOS = 74
+
+const Snapshots = () => {
+  const photos = useMemo(() => {
+    const output = []
+    for (let i = 1; i <= TOTAL_PHOTOS; i++) output.push(<Snapshot key={i} src={`/public/snapshots/snapshot-${i}.jpg`} />)
+    return shuffledArray(output)
+  }, [])
+
+  return (
+    <div>
+      {photos}
+    </div>
+  )
+}
+
+export default Snapshots
