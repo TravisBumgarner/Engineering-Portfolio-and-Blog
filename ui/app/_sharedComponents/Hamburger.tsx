e8eb223c2ead76c417bc11042fbcb0f809cdea09
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-30 17:45:33 -0600

    hamburger is nice

---
+++ ui/app/_sharedComponents/Hamburger.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { THEME } from '@/lib/theme'
+import styled from 'styled-components'
+
+const Bar = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: ${THEME.PRIMARY_COLOR};
+  box-sizing: border-box;
+`
+
+const Wrapper = styled.div`
+  width: 30px;
+  height: 30px;
+  box-sizing: border-box;
+  background-color: ${THEME.SECONDARY_BACKGROUND_COLOR};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 5px solid ${THEME.SECONDARY_BACKGROUND_COLOR};
+`
+
+const Hamburger = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <Wrapper onClick={onClick}>
+      <Bar />
+      <Bar />
+      <Bar />
+    </Wrapper>
+  )
+}
+
+export default Hamburger
