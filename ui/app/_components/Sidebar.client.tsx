c9f16a8968db1429c90b877122e5665857e67d51
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-30 18:14:02 -0600

    fun

--- ui/app/_components/Sidebar.client.tsx
+++ ui/app/_components/Sidebar.client.tsx
@@ -2,11 +2,9 @@
 
 import ROUTES from '@/lib/routes'
 import { SPACING, THEME } from '@/lib/theme'
-import { motion } from 'framer-motion'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import Hamburger from '../_sharedComponents/Hamburger'
 
 const THERE = [
   {
@@ -31,54 +29,78 @@ const THERE = [
   }
 ]
 
-const SidebarClient = () => {
-  const [isOpen, setIsOpen] = useState(false)
+const Item = ({
+  title,
+  path,
+  target
+}: {
+  title: string
+  path: string
+  target: string
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const [displayText, setDisplayText] = useState(title.slice(0, 1))
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout
+    let currentIndex = isHovered ? 1 : title.length
+
+    const animateText = () => {
+      if (isHovered) {
+        if (currentIndex <= title.length) {
+          setDisplayText(title.slice(0, currentIndex))
+          currentIndex++
+          timeoutId = setTimeout(animateText, 25)
+        }
+      } else {
+        if (currentIndex > 0) {
+          setDisplayText(title.slice(0, currentIndex))
+          currentIndex--
+          timeoutId = setTimeout(animateText, 25)
+        }
+      }
+    }
+    animateText()
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [isHovered, title])
 
   return (
+    <Box
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Link target={target} href={path}>
+        {displayText}
+      </Link>
+    </Box>
+  )
+}
+
+const SidebarClient = () => {
+  return (
     <Positioner>
-      <Wrapper>
-        
-        <motion.div>
-          <motion.ul
-            initial={{ transform: 'scale(0)' }}
-            animate={{ transform: isOpen ? 'scale(1)' : 'scale(0)' }}
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-          >
-            {Object.values(ROUTES).map(r => (
-              <Box key={r.path}>
-                <Link href={r.path}>{r.title}</Link>
-              </Box>
-            ))}
-          </motion.ul>
-          <Hamburger onClick={() => setIsOpen(!isOpen)} />
-          <motion.ul
-            initial={{ transform: 'scale(0)' }}
-            animate={{ transform: isOpen ? 'scale(1)' : 'scale(0)' }}
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-          >
-            {THERE.map(t => (
-              <Box key={t.path}>
-                <Link target={t.target} href={t.path}>
-                  {t.title}
-                </Link>
-              </Box>
-            ))}
-          </motion.ul>
-        </motion.div>
-      </Wrapper>
+        {[...Object.values(ROUTES), ...THERE].map(r => (
+          <Item key={r.path} title={r.title} path={r.path} target={r.target} />
+        ))}
     </Positioner>
   )
 }
 
-const Box = styled.li`
-  padding: 0 10px;
-  height: 50px;
-  margin: ${SPACING.XSMALL} 0;
+const Box = styled.div`
+  align-self: flex-start;
+  min-width: 30px;
+  height: 30px;
+  margin: ${SPACING.XXSMALL} 0;
   background-color: ${THEME.SECONDARY_BACKGROUND_COLOR};
   color: ${THEME.FOREGROUND_COLOR};
-  display: flex;
-  align-items: center;
-  justify-content: center;
+
+  text-align: left;
+  padding-left: 10px;
+  padding-right: 10px;
+  align-content: center;
 
   a {
     text-decoration: none;
@@ -92,12 +114,10 @@ const Positioner = styled.div`
   top: 0;
   left: 0;
   height: 100%;
-  align-content: center;
-`
-
-const Wrapper = styled(motion.div)`
-  background: ${THEME.PRIMARY_BACKGROUND_COLOR};
-  padding: 10px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
 `
 
 export default SidebarClient
