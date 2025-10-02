117a02795806dcb0879cd62f5bab39cd3fe4717d
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-10-02 14:41:36 -0600

    Post corrections (#99)
    * done
    
    * clean up subscribe

--- ui/app/blog/Subscribe.tsx
+++ ui/app/blog/Subscribe.tsx
@@ -6,33 +6,27 @@ import styled from 'styled-components'
 import Button from '../_sharedComponents/Button'
 import { Input } from '../_sharedComponents/Input'
 
-const SubWrapper = styled.div`
+const Wrapper = styled.div`
+  margin-bottom: ${SPACING.MEDIUM};
+  padding: ${SPACING.XSMALL};
+  background-color: var(--bright-background);
   display: flex;
   flex-direction: row;
   gap: ${SPACING.SMALL};
   align-items: center;
   justify-content: center;
 
-  @media (max-width: 768px) {
+  a,
+  input,
+  button {
+    height: 36px;
+    font-size: ${FONT_SIZES.SMALL};
+    margin: 0;
+  }
+
+  @media (max-width: 400px) {
     flex-direction: column;
     gap: ${SPACING.XSMALL};
-  }
-
-  a {
-    width: 120px;
-  }
-`
-
-const Wrapper = styled.div`
-  margin-bottom: ${SPACING.MEDIUM};
-  padding: ${SPACING.MEDIUM};
-  background-color: var(--bright-background);
-
-  h2 {
-    font-size: ${FONT_SIZES.LARGE};
-    font-weight: ${FONT_WEIGHTS.BOLD};
-    margin-top: 0;
-    margin-bottom: ${SPACING.SMALL};
   }
 `
 
@@ -43,14 +37,6 @@ const Form = styled.form`
   width: 100%;
   input {
     flex-grow: 1;
-  }
-
-  @media (max-width: 768px) {
-    input,
-    button {
-      font-size: ${FONT_SIZES.SMALL};
-      padding: ${SPACING.XSMALL};
-    }
   }
 `
 
@@ -63,61 +49,64 @@ const Subscribe = () => {
 
   return (
     <Wrapper>
-      <SubWrapper>
-        <RSSLink href="/rss.xml" target="_blank" rel="noopener noreferrer">
-          <svg
-            style={{ position: 'relative', top: '2px' }}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="rss-icon"
-          >
-            <path d="M4 11a9 9 0 0 1 9 9" />
-            <path d="M4 4a16 16 0 0 1 16 16" />
-            <circle cx="5" cy="19" r="1" />
-          </svg>
-          <span style={{ position: 'relative', top: '-3px', left: '5px' }}>
-            RSS
-          </span>
-        </RSSLink>
-        <p>Or</p>
-        <Form
-          action="https://buttondown.com/api/emails/embed-subscribe/tbumgarner"
-          method="post"
-          target="popupwindow"
-          onSubmit={handleSubmit}
-          className="embeddable-buttondown-form"
+      <RSSLink href="/rss.xml" target="_blank" rel="noopener noreferrer">
+        <svg
+          style={{ position: 'relative', top: '2px' }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="rss-icon"
         >
-          <Input
-            placeholder="Email"
-            type="email"
-            name="email"
-            id="bd-email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            style={{ borderRadius: 0 }}
-          />
-          <Button type="submit" disabled={email.length === 0}>
-            Subscribe
-          </Button>
-        </Form>
-      </SubWrapper>
+          <path d="M4 11a9 9 0 0 1 9 9" />
+          <path d="M4 4a16 16 0 0 1 16 16" />
+          <circle cx="5" cy="19" r="1" />
+        </svg>
+        <span>RSS</span>
+      </RSSLink>
+      <p>Or</p>
+      <Form
+        action="https://buttondown.com/api/emails/embed-subscribe/tbumgarner"
+        method="post"
+        target="popupwindow"
+        onSubmit={handleSubmit}
+        className="embeddable-buttondown-form"
+      >
+        <Input
+          placeholder="Email"
+          type="email"
+          name="email"
+          id="bd-email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          style={{ borderRadius: 0 }}
+        />
+        <Button
+          style={{ padding: `0 8px` }}
+          type="submit"
+          disabled={email.length === 0}
+        >
+          Subscribe
+        </Button>
+      </Form>
     </Wrapper>
   )
 }
 
 const RSSLink = styled.a`
   background-color: var(--secondary-background);
-  margin: ${SPACING.XSMALL} 0;
-  width: 100%;
-  display: block;
-  padding: ${SPACING.SMALL};
+  width: 120px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: ${SPACING.XSMALL};
+  border: 0;
   color: var(--primary);
   text-decoration: none;
   font-weight: ${FONT_WEIGHTS.REGULAR};
