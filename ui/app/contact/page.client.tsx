397d01f6b96cff716881867451eaa7dfddc47859
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-10-19 16:19:37 -0600

    can inject content into contact form

--- ui/app/contact/page.client.tsx
+++ ui/app/contact/page.client.tsx
@@ -3,12 +3,19 @@
 import React from 'react'
 import ContentStyler from '../../lib/sharedComponents/ContentStyler'
 import ContactForm from '../../lib/sharedComponents/ContactForm'
+import { useSearchParams } from 'next/navigation'
 
 const Contact = () => {
+  const searchParams = useSearchParams()
+  const rawSubject = searchParams.get('subject')
+  const subject = rawSubject
+    ? rawSubject.replace(/[^a-zA-Z0-9 :]/g, '').slice(0, 100)
+    : undefined
+
   return (
     <ContentStyler>
       <h2>Let&apos;s Chat</h2>
-      <ContactForm />
+      <ContactForm subject={subject} />
     </ContentStyler>
   )
 }
