1e7b4143942bb9ca940e9aa0983a0e57cfb3a481
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-11-25 22:38:57 -0600

    started using mui in mdx

--- ui/lib/mdxLibrary.tsx
+++ ui/lib/mdxLibrary.tsx
@@ -1,13 +1,94 @@
 'use client'
 
 import Typography from '@mui/material/Typography'
+import { ComponentProps } from 'react'
+import Link from '@/lib/sharedComponents/Link'
 
-
-// Add whatever you want to override
 export const mdxComponents = {
-    h1: (props: unknown) => <Typography variant="h2" sx={{ fontSize: '3rem', border: '2px solid red' }} {...props} />,
-    h2: (props: unknown) => <Typography variant="h3" sx={{ fontSize: '3rem', border: '2px solid red' }} {...props} />,
-    h3: (props: unknown) => <Typography variant="h4" sx={{ fontSize: '3rem', border: '2px solid red' }} gutterBottom {...props} />,
-    h4: (props: unknown) => <Typography variant="h5" sx={{ fontSize: '3rem', border: '2px solid red' }} gutterBottom {...props} />,
+    h1: ({ children, id, className }: ComponentProps<'h1'>) => (
+        <Typography
+            component="h1"
+            variant="h1"
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    h2: ({ children, id, className }: ComponentProps<'h2'>) => (
+        <Typography
+            component="h2"
+            variant="h2"
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    h3: ({ children, id, className }: ComponentProps<'h3'>) => (
+        <Typography
+            component="h3"
+            variant="h3"
+            gutterBottom
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    h4: ({ children, id, className }: ComponentProps<'h4'>) => (
+        <Typography
+            component="h4"
+            variant="h4"
+            gutterBottom
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    h5: ({ children, id, className }: ComponentProps<'h5'>) => (
+        <Typography
+            component="h5"
+            variant="h5"
+            gutterBottom
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    h6: ({ children, id, className }: ComponentProps<'h6'>) => (
+        <Typography
+            component="h6"
+            variant="h6"
+            gutterBottom
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    p: ({ children, id, className }: ComponentProps<'p'>) => (
+        <Typography
+            component="p"
+            variant="body1"
+            id={id}
+            className={className}
+        >
+            {children}
+        </Typography>
+    ),
+    a: ({ children, href, id, className, ...props }: ComponentProps<'a'>) => (
+        <Link
+            variant="inline"
+            href={href || ''}
+            id={id}
+            className={className}
+            {...props}
+        >
+            {children}
+        </Link>
+    )
 }
 
