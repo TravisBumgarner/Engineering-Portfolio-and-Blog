520a8eacfddb7cdc4036a6a6bb60ebdee7aeb56a
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2026-01-20 15:55:25 -0600

    Delete nextjs

--- old-nextjs/app/contact/page.tsx
+++
@@ -1,5 +0,0 @@
-import Contact from './page.client'
-
-export default function ContactPage() {
-  return <Contact />
-}
--- old-nextjs/app/loading.tsx
+++
@@ -1,3 +0,0 @@
-import Loading from '../lib/sharedComponents/Loading'
-
-export default Loading
