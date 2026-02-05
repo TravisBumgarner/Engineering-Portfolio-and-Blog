76e030e4d67e6b305d7538aa456f92172a4f748e
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2026-02-05 13:29:45 -0600

    done

--- frontend/src/components/Scroll.tsx
+++ frontend/src/components/Scroll.tsx
@@ -4,13 +4,23 @@ import { useLocation } from 'react-router-dom'
 export default function ScrollToTop() {
   const { pathname, hash } = useLocation()
 
+  // This feels like a bit of a bandaid.
+  // We need to wait for the element to be in the DOM which may take some time.
+  // So we try a few times before giving up.
+
   useEffect(() => {
     if (hash) {
-      const element = document.querySelector(hash)
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' })
-        return
-      }
+      let attempts = 0
+      const interval = setInterval(() => {
+        const element = document.getElementById(hash.substring(1))
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' })
+          clearInterval(interval)
+        } else if (++attempts >= 8) {
+          clearInterval(interval)
+        }
+      }, 250)
+      return () => clearInterval(interval)
     }
     window.scrollTo(0, 0)
   }, [pathname, hash])
